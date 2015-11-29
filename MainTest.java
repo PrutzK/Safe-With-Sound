968339e58308edc5d91cc41e6525240e456a7933
Katherine Prutz <prutz.katherine@gmail.com>
2015-11-28 23:40:52 -0500

    Create MainTest.java

---
+++ MainTest.java
@@ -0,0 +1,21 @@
+package sws;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        List<Double> parent1Data = Arrays.asList(10.5,13.,14.,24.,50.); 
+        Person parent1 = new Person(parent1Data);
+        
+        
+        
+    }
+
+}
