cbed1edbb93e09ca0cc69039a4db225d5641ba93
Katherine Prutz <prutz.katherine@gmail.com>
2015-11-28 23:39:27 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+package sws;
+
+/**
+ * Read in a file of a data and get a list of probabilities that the data corresponds to a certain person. 
+ * @author katherineprutz
+ */
+public class main {
+    
+}
